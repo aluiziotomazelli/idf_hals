c83ebd7ea2dfb3d6d2f6201257e9652c8f3e2159
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-07-20 23:27:39 -0400

    fix(hal_sleep): use soc_caps.h macros for portable deep sleep GPIO wakeup

--- src/hal_sleep.cpp
+++ src/hal_sleep.cpp
@@ -16,15 +16,18 @@ esp_err_t SleepHAL::enable_timer_wakeup(uint64_t time_in_us)
 }
 
 #include "sdkconfig.h"
+#include "soc/soc_caps.h"
 
 esp_err_t SleepHAL::deep_sleep_enable_gpio_wakeup(uint64_t gpio_pin_mask, GpioWakeupMode mode)
 {
-#if CONFIG_IDF_TARGET_ESP32
+#if SOC_GPIO_SUPPORT_DEEP_SLEEP_WAKEUP
+    esp_deepsleep_gpio_wake_up_mode_t gpio_mode = (mode == GpioWakeupMode::HIGH_LEVEL) ? ESP_GPIO_WAKEUP_GPIO_HIGH : ESP_GPIO_WAKEUP_GPIO_LOW;
+    return esp_deep_sleep_enable_gpio_wakeup(gpio_pin_mask, gpio_mode);
+#elif SOC_PM_SUPPORT_EXT1_WAKEUP
     esp_sleep_ext1_wakeup_mode_t ext1_mode = (mode == GpioWakeupMode::HIGH_LEVEL) ? ESP_EXT1_WAKEUP_ANY_HIGH : ESP_EXT1_WAKEUP_ALL_LOW;
     return esp_sleep_enable_ext1_wakeup(gpio_pin_mask, ext1_mode);
 #else
-    esp_deepsleep_gpio_wake_up_mode_t gpio_mode = (mode == GpioWakeupMode::HIGH_LEVEL) ? ESP_GPIO_WAKEUP_GPIO_HIGH : ESP_GPIO_WAKEUP_GPIO_LOW;
-    return esp_deep_sleep_enable_gpio_wakeup(gpio_pin_mask, gpio_mode);
+    return ESP_ERR_NOT_SUPPORTED;
 #endif
 }
 
